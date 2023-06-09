eda523f44539e3e39139f8213c00c2fb3db90125
AbirChoudhury-26 <abirudaipur@gmail.com>
2023-06-09 22:39:24 +0530

    Code Complete

--- Min-Spanning-Tree.cpp
+++ Min-Spanning-Tree.cpp
@@ -4,9 +4,43 @@
 #include<bits/stdc++.h>
 using namespace std;
  
-int spanningtree(int v,vector<vector<int>>adj[])
+int spanningtree(int V,vector<vector<int>>adj[])
 {
-    
+    priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>pq; // Priority based on Small Values & less weight;
+
+    vector<int>vis(V,0);
+
+   // {wt,node}
+pq.push({0,0});
+
+int sum=0;  // Sum with total  Minimum weight values
+
+ while(!pq.empty())
+ {
+  auto it=pq.top();
+  pq.pop();
+
+   int node=it.second;
+   int weight=it.first;
+
+    if(vis[node]==1)  // To check if node is alreday visisted or not
+     continue;
+
+    vis[node]=1;
+
+     sum+=weight;
+      
+      for(auto it:adj[node])
+      {
+        int adjnode=it[0];
+        int edw=it[1];
+         if(!vis[adjnode])
+          pq.push({edw,adjnode});
+      }
+ }
+
+return sum;
+
 }
 int main()
 {
