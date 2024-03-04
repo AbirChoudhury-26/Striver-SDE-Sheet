4b1ca95c20e20eb17ecc4d6e7303121a966df3be
AbirChoudhury-26 <abirudaipur@gmail.com>
2024-03-04 20:48:45 +0530

    New App

--- TCS/Arrays/K-Rotation-Array.cpp
+++ TCS/Arrays/K-Rotation-Array.cpp
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// Function to rotate array
+/*  Using Temp array*/
 void Rotate(int arr[], int d, int n)
 {
 	// Storing rotated version of array
@@ -50,6 +50,51 @@ int main()
 	// Function calling
 	Rotate(arr, d, N);
 	PrintTheArray(arr, N);
+
+	return 0;
+}
+
+
+// Method-2 
+
+/* Rotate One by One*/
+
+// C++ program to rotate an array by
+// d elements
+#include <bits/stdc++.h>
+using namespace std;
+
+/*Function to left rotate arr[] of size n by d*/
+void Rotate(int arr[], int d, int n)
+{
+	int p = 1;
+	while (p <= d) {
+		int last = arr[0];
+		for (int i = 0; i < n - 1; i++) {
+			arr[i] = arr[i + 1];
+		}
+		arr[n - 1] = last;
+		p++;
+	}
+}
+
+// Function to print an array
+void printArray(int arr[], int size)
+{
+	for (int i = 0; i < size; i++)
+		cout << arr[i] << " ";
+}
+
+// Driver code
+int main()
+{
+	int arr[] = { 1, 2, 3, 4, 5, 6, 7 };
+	int N = sizeof(arr) / sizeof(arr[0]);
+	int d = 2;
+
+	// Function calling
+	Rotate(arr, d, N);
+	printArray(arr, N);
 
 	return 0;
 }
