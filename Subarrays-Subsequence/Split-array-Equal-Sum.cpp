c017293b0c44e00fcd3ea65976e1d64781bd2125
AbirChoudhury-26 <abirudaipur@gmail.com>
2024-03-13 21:32:48 +0530

    New

--- Subarrays-Subsequence/Split-array-Equal-Sum.cpp
+++ Subarrays-Subsequence/Split-array-Equal-Sum.cpp
@@ -1,5 +1,68 @@
-// Naive Appproach
+// // Naive Appproach
 
+// #include<bits/stdc++.h> 
+// using namespace std; 
+
+// // Returns split point. If not possible, then 
+// // return -1. 
+// int findSplitPoint(int arr[], int n) 
+// { 
+// 	int leftSum = 0 ; 
+
+// 	// traverse array element 
+// 	for (int i = 0; i < n; i++) 
+// 	{ 
+// 		// add current element to left Sum 
+// 		leftSum += arr[i] ; 
+
+// 		// find sum of rest array elements (rightSum) 
+// 		int rightSum = 0 ; 
+// 		for (int j = i+1 ; j < n ; j++ ) 
+// 			rightSum += arr[j] ; 
+
+// 		// split point index 
+// 		if (leftSum == rightSum) 
+// 			return i+1 ; 
+// 	} 
+
+// 	// if it is not possible to split array into 
+// 	// two parts 
+// 	return -1; 
+// } 
+
+// // Prints two parts after finding split point using 
+// // findSplitPoint() 
+// void printTwoParts(int arr[], int n) 
+// { 
+// 	int splitPoint = findSplitPoint(arr, n); 
+
+// 	if (splitPoint == -1 || splitPoint == n ) 
+// 	{ 
+// 		cout << "Not Possible" <<endl; 
+// 		return; 
+// 	} 
+// 	for (int i = 0; i < n; i++) 
+// 	{ 
+// 		if(splitPoint == i) 
+// 			cout << endl; 
+// 		cout << arr[i] << " " ; 
+// 	} 
+// } 
+
+// // driver program 
+// int main() 
+// { 
+// 	int arr[] = {1 , 2 , 3 , 4 , 5 , 5 }; 
+// 	int n = sizeof(arr)/sizeof(arr[0]); 
+// 	printTwoParts(arr, n); 
+// 	return 0; 
+// } 
+
+
+// Efficient Soln
+
+// C++ program to split an array into Two 
+// equal sum subarrays 
 #include<bits/stdc++.h> 
 using namespace std; 
 
@@ -7,26 +70,30 @@ using namespace std;
 // return -1. 
 int findSplitPoint(int arr[], int n) 
 { 
-	int leftSum = 0 ; 
+	// traverse array element and compute sum 
+	// of whole array 
+	int leftSum = 0; 
+	for (int i = 0 ; i < n ; i++) 
+		leftSum += arr[i]; 
 
-	// traverse array element 
-	for (int i = 0; i < n; i++) 
+	// again traverse array and compute right sum 
+	// and also check left_sum equal to right 
+	// sum or not 
+	int rightSum = 0; 
+	for (int i=n-1; i >= 0; i--) 
 	{ 
-		// add current element to left Sum 
-		leftSum += arr[i] ; 
+		// add current element to right_sum 
+		rightSum += arr[i]; 
 
-		// find sum of rest array elements (rightSum) 
-		int rightSum = 0 ; 
-		for (int j = i+1 ; j < n ; j++ ) 
-			rightSum += arr[j] ; 
+		// exclude current element to the left_sum 
+		leftSum -= arr[i] ; 
 
-		// split point index 
-		if (leftSum == rightSum) 
-			return i+1 ; 
+		if (rightSum == leftSum) 
+			return i ; 
 	} 
 
-	// if it is not possible to split array into 
-	// two parts 
+	// if it is not possible to split array 
+	// into two parts. 
 	return -1; 
 } 
 
