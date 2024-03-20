2703122da024368fa1d393c1aee6a906a718c01b
AbirChoudhury-26 <abirudaipur@gmail.com>
2024-03-20 22:46:08 +0530

    New

--- TCS/Arrays/Longest-Increasing-Subsequence.cpp
+++ TCS/Arrays/Longest-Increasing-Subsequence.cpp
@@ -1,32 +1,30 @@
-#include<iostream>
-#include<bits/stdc++.h>
+#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
- 
+
 int main()
 {
   int n;
-   cin>>n;
-   vector<int>v(n);
-    for(int i=0;i<n;i++)
-     cin>>v[i];
+  cin >> n;
+  vector<int> v(n);
+  for (int i = 0; i < n; i++)
+    cin >> v[i];
 
-    int s=0,e=0,ans=1;
-      for(int i=1;i<n;i++)
-      {
-         if(v[i]>v[e]) 
-         {
-          e=i;
-          ans=max(ans,(e-s)+1);
-         }
-        else
-        {
-         s=i,
-         e=i;
-        }
+  int s = 0, e = 0, ans = 1;
+  for (int i = 1; i < n; i++)
+  {
+    if (v[i] > v[e])
+    {
+      e = i;
+      ans = max(ans, (e - s) + 1);
+    }
+    else
+    {
+      s = i,
+      e = i;
+    }
+  }
 
-        
-      }
-
-      cout<<ans;
+  cout << ans;
   return 0;
 }
