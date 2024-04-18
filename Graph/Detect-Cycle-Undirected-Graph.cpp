3645eb680ee0739679009336a737612df272a591
AbirChoudhury-26 <abirudaipur@gmail.com>
2024-04-18 21:53:36 +0530

    New

--- Graph/Detect-Cycle-Undirected-Graph.cpp
+++ Graph/Detect-Cycle-Undirected-Graph.cpp
@@ -1,5 +1,15 @@
-// A C++ Program to detect
-// cycle in an undirected graph
+/* Approach:
+
+Iterate over all the nodes of the graph and Keep a visited array visited[] to track the visited nodes.
+Run a Depth First Traversal on the given subgraph connected to the current node and pass the parent of the current node. In each recursive 
+Set visited[root] as 1.
+Iterate over all adjacent nodes of the current node in the adjacency list 
+If it is not visited then run DFS on that node and return true if it returns true.
+Else if the adjacent node is visited and not the parent of the current node then return true.
+Return false.
+
+*/
+
 #include <iostream>
 #include <limits.h>
 #include <list>
