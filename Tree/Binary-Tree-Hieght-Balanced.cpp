98c4a8b2e90957c35ed53196d6f9180dd0496e8b
AbirChoudhury-26 <abirudaipur@gmail.com>
2024-05-02 23:37:37 +0530

    new

--- Tree/Binary-Tree-Hieght-Balanced.cpp
+++ Tree/Binary-Tree-Hieght-Balanced.cpp
@@ -1,78 +1,130 @@
-/* Approach:
+// /* Approach 1:
 
-Get the height of left and right subtrees using dfs traversal. 
-Return true if the difference between heights is not more than 1.
-Also left and right subtrees are balanced, otherwise return false.
+// Get the height of left and right subtrees using dfs traversal. 
+// Return true if the difference between heights is not more than 1.
+// Also left and right subtrees are balanced, otherwise return false.
 
-*/
+// */
+
+// #include <bits/stdc++.h>
+// using namespace std;
+
+
+// class Node {
+// public:
+// 	int data;
+// 	Node* left;
+// 	Node* right;
+// 	Node(int d)
+// 	{
+// 		int data = d;
+// 		left = right = NULL;
+// 	}
+// };
+
+// int height(Node* node)
+// {
+// 	// base case tree is empty
+// 	if (node == NULL)
+// 		return 0;
+
+// 	// If tree is not empty then
+// 	// height = 1 + max of left height
+// 	// and right heights
+// 	return 1 + max(height(node->left), height(node->right));
+// }
+
+
+// bool isBalanced(Node* root)
+// {
+// 	// for height of left subtree
+// 	int lh;
+
+// 	// for height of right subtree
+// 	int rh;
+
+// 	// If tree is empty then return true
+// 	if (root == NULL)
+// 		return 1;
+
+// 	// Get the height of left and right sub trees
+// 	lh = height(root->left);
+// 	rh = height(root->right);
+
+// 	if (abs(lh - rh) <= 1 && isBalanced(root->left)
+// 		&& isBalanced(root->right))
+// 		return 1;
+
+// 	// If we reach here then tree is not height-balanced
+// 	return 0;
+// }
+
+// // Driver code
+// int main()
+// {
+// 	Node* root = new Node(1);
+// 	root->left = new Node(2);
+// 	root->right = new Node(3);
+// 	root->left->left = new Node(4);
+// 	root->left->right = new Node(5);
+// 	root->left->left->left = new Node(8);
+
+// 	if (isBalanced(root))
+// 		cout << "Tree is balanced";
+// 	else
+// 		cout << "Tree is not balanced";
+// 	return 0;
+// }
+
+
+// Method-2 : Using Recusrion Only
+
 
 #include <bits/stdc++.h>
 using namespace std;
 
 
-class Node {
-public:
-	int data;
-	Node* left;
-	Node* right;
-	Node(int d)
+struct Node {
+	int key;
+	struct Node* left;
+	struct Node* right;
+	Node(int k)
 	{
-		int data = d;
+		key = k;
 		left = right = NULL;
 	}
 };
 
-int height(Node* node)
+// Function to check if tree is height balanced
+int isBalanced(Node* root)
 {
-	// base case tree is empty
-	if (node == NULL)
-		return 0;
-
-	// If tree is not empty then
-	// height = 1 + max of left height
-	// and right heights
-	return 1 + max(height(node->left), height(node->right));
-}
-
-
-bool isBalanced(Node* root)
-{
-	// for height of left subtree
-	int lh;
-
-	// for height of right subtree
-	int rh;
-
-	// If tree is empty then return true
 	if (root == NULL)
-		return 1;
+		return 0;
+	int lh = isBalanced(root->left);
+	if (lh == -1)
+		return -1;
+	int rh = isBalanced(root->right);
+	if (rh == -1)
+		return -1;
 
-	// Get the height of left and right sub trees
-	lh = height(root->left);
-	rh = height(root->right);
-
-	if (abs(lh - rh) <= 1 && isBalanced(root->left)
-		&& isBalanced(root->right))
-		return 1;
-
-	// If we reach here then tree is not height-balanced
-	return 0;
+	if (abs(lh - rh) > 1)
+		return -1;
+	else
+		return max(lh, rh) + 1;
 }
 
-// Driver code
+
 int main()
 {
-	Node* root = new Node(1);
-	root->left = new Node(2);
-	root->right = new Node(3);
-	root->left->left = new Node(4);
-	root->left->right = new Node(5);
-	root->left->left->left = new Node(8);
+	Node* root = new Node(10);
+	root->left = new Node(5);
+	root->right = new Node(30);
+	root->right->left = new Node(15);
+	root->right->right = new Node(20);
 
-	if (isBalanced(root))
-		cout << "Tree is balanced";
+	if (isBalanced(root) > 0)
+		cout << "Balanced";
 	else
-		cout << "Tree is not balanced";
+		cout << "Not Balanced";
 	return 0;
 }
-
