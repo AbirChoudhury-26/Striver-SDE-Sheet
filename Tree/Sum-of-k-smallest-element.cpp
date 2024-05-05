fd73f1d3982b174302213665a537c662a7d00f73
AbirChoudhury-26 <abirudaipur@gmail.com>
2024-05-05 13:37:02 +0530

    New

--- Tree/Sum-of-k-smallest-element.cpp
+++ Tree/Sum-of-k-smallest-element.cpp
@@ -1,3 +1,96 @@
+// /* Method 1 : */
+
+// #include <bits/stdc++.h>
+// using namespace std;
+
+
+// struct Node
+// {
+// 	int data;
+// 	Node* left, * right;
+// };
+
+
+// struct Node *createNode(int data)
+// {
+// 	Node * new_Node = new Node;
+// 	new_Node->left = NULL;
+// 	new_Node->right = NULL;
+// 	new_Node->data = data;
+// 	return new_Node;
+// }
+
+
+// struct Node * insert(Node *root, int key)
+// {
+	
+// 	if (root == NULL)
+// 		return createNode(key);
+
+	
+// 	if (root->data > key)
+// 		root->left = insert(root->left, key);
+
+// 	else if (root->data < key)
+// 		root->right = insert(root->right, key);
+
+	
+// 	return root;
+// }
+
+
+// int ksmallestElementSumRec(Node *root, int k, int &count)
+// {
+	
+// 	if (root == NULL)
+// 		return 0;
+// 	if (count > k)
+// 		return 0;
+
+	
+// 	int res = ksmallestElementSumRec(root->left, k, count);
+// 	if (count >= k)
+// 		return res;
+
+
+// 	res += root->data;
+// 	count++;
+// 	if (count >= k)
+// 	return res;
+
+	
+// 	return res + ksmallestElementSumRec(root->right, k, count);
+// }
+
+
+// int ksmallestElementSum(struct Node *root, int k)
+// {
+// int count = 0;
+// return ksmallestElementSumRec(root, k, count);
+// }
+
+
+// int main()
+// {
+
+// 	Node *root = NULL;
+// 	root = insert(root, 20);
+// 	root = insert(root, 8);
+// 	root = insert(root, 4);
+// 	root = insert(root, 12);
+// 	root = insert(root, 10);
+// 	root = insert(root, 14);
+// 	root = insert(root, 22);
+
+// 	int k = 3;
+// 	cout << ksmallestElementSum(root, k) <<endl;
+// 	return 0;
+// }
+
+
+// Method 2: Efficient manner 
+
+
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -5,7 +98,10 @@ using namespace std;
 struct Node
 {
 	int data;
-	Node* left, * right;
+	int lCount;
+	int Sum ;
+	Node* left;
+	Node* right;
 };
 
 
@@ -15,9 +111,10 @@ struct Node *createNode(int data)
 	new_Node->left = NULL;
 	new_Node->right = NULL;
 	new_Node->data = data;
+	new_Node->lCount = 0 ;
+	new_Node->Sum = 0;
 	return new_Node;
 }
-
 
 struct Node * insert(Node *root, int key)
 {
@@ -25,52 +122,59 @@ struct Node * insert(Node *root, int key)
 	if (root == NULL)
 		return createNode(key);
 
-	
-	if (root->data > key)
-		root->left = insert(root->left, key);
 
-	else if (root->data < key)
-		root->right = insert(root->right, key);
+	if (root->data > key)
+	{
+		
+		root->lCount++;
+		root->Sum += key;
+
+		root->left= insert(root->left , key);
+	}
+	else if (root->data < key )
+		root->right= insert (root->right , key );
 
 	
 	return root;
 }
 
 
-int ksmallestElementSumRec(Node *root, int k, int &count)
+void ksmallestElementSumRec(Node *root, int k , int &temp_sum)
 {
-	// Base cases
 	if (root == NULL)
-		return 0;
-	if (count > k)
-		return 0;
+		return ;
 
 	
-	int res = ksmallestElementSumRec(root->left, k, count);
-	if (count >= k)
-		return res;
+	if ((root->lCount + 1) == k)
+	{
+		temp_sum += root->data + root->Sum ;
+		return ;
+	}
 
+	else if (k > root->lCount)
+	{
+		
+		temp_sum += root->data + root->Sum;
 
-	res += root->data;
-	count++;
-	if (count >= k)
-	return res;
-
-	
-	return res + ksmallestElementSumRec(root->right, k, count);
+		
+		k = k -( root->lCount + 1);
+		ksmallestElementSumRec(root->right , k , temp_sum);
+	}
+	else 
+		ksmallestElementSumRec(root->left , k , temp_sum );
 }
 
 
 int ksmallestElementSum(struct Node *root, int k)
 {
-int count = 0;
-return ksmallestElementSumRec(root, k, count);
+	int sum = 0;
+	ksmallestElementSumRec(root, k, sum);
+	return sum;
 }
 
 
 int main()
 {
-
 	Node *root = NULL;
 	root = insert(root, 20);
 	root = insert(root, 8);
@@ -81,6 +185,6 @@ int main()
 	root = insert(root, 22);
 
 	int k = 3;
-	cout << ksmallestElementSum(root, k) <<endl;
+	cout << ksmallestElementSum(root, k) << endl;
 	return 0;
 }
