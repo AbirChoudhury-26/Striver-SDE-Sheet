e9a13b9e21cb03a0d66bce13d445ba9f6f6dfbb1
AbirChoudhury-26 <abirudaipur@gmail.com>
2024-05-12 10:24:29 +0530

    New

--- Tree/Pair-with-Given-Sum-BST.cpp
+++ Tree/Pair-with-Given-Sum-BST.cpp
@@ -8,65 +8,149 @@ If for a current node with value x, there exists a y for which x + y = sum then 
 */
 
 
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// struct Node {
+// 	int data;
+// 	struct Node *left, *right;
+// };
+
+// Node* NewNode(int data)
+// {
+// 	Node* temp = (Node*)malloc(sizeof(Node));
+// 	temp->data = data;
+// 	temp->left = NULL;
+// 	temp->right = NULL;
+// 	return temp;
+// }
+
+// Node* insert(Node* root, int key)
+// {
+// 	if (root == NULL)
+// 		return NewNode(key);
+// 	if (key < root->data)
+// 		root->left = insert(root->left, key);
+// 	else
+// 		root->right = insert(root->right, key);
+// 	return root;
+// }
+
+// bool findpairUtil(Node* root, int sum,
+// 				unordered_set<int>& set)
+// {
+// 	if (root == NULL)
+// 		return false;
+
+// 	if (findpairUtil(root->left, sum, set))
+// 		return true;
+
+// 	if (set.find(sum - root->data) != set.end()) {
+// 		cout << "Pair is found (" << sum - root->data
+// 			<< ", " << root->data << ")" << endl;
+// 		return true;
+// 	}
+// 	else
+// 		set.insert(root->data);
+
+// 	return findpairUtil(root->right, sum, set);
+// }
+
+// void findPair(Node* root, int sum)
+// {
+// 	unordered_set<int> set;
+// 	if (!findpairUtil(root, sum, set))
+// 		cout << "Pairs do not exit" << endl;
+// }
+
+
+// int main()
+// {
+// 	Node* root = NULL;
+// 	root = insert(root, 15);
+// 	root = insert(root, 10);
+// 	root = insert(root, 20);
+// 	root = insert(root, 8);
+// 	root = insert(root, 12);
+// 	root = insert(root, 16);
+// 	root = insert(root, 25);
+// 	root = insert(root, 10);
+
+// 	int sum = 28;
+// 	findPair(root, sum);
+
+// 	return 0;
+// }
+
+
+// Method-2 : Using 2 Pointer Approach
+
+
+
 #include <bits/stdc++.h>
 using namespace std;
 
-struct Node {
-	int data;
-	struct Node *left, *right;
+struct TreeNode {
+	int val;
+	struct TreeNode *left, *right;
 };
 
-Node* NewNode(int data)
+TreeNode* NewNode(int data)
 {
-	Node* temp = (Node*)malloc(sizeof(Node));
-	temp->data = data;
+	TreeNode* temp = (TreeNode*)malloc(sizeof(TreeNode));
+	temp->val = data;
 	temp->left = NULL;
 	temp->right = NULL;
 	return temp;
 }
 
-Node* insert(Node* root, int key)
+TreeNode* insert(TreeNode* root, int key)
 {
 	if (root == NULL)
 		return NewNode(key);
-	if (key < root->data)
+	if (key < root->val)
 		root->left = insert(root->left, key);
 	else
 		root->right = insert(root->right, key);
 	return root;
 }
 
-bool findpairUtil(Node* root, int sum,
-				unordered_set<int>& set)
+void inorder(TreeNode* root, vector<int>& v)
 {
-	if (root == NULL)
-		return false;
-
-	if (findpairUtil(root->left, sum, set))
-		return true;
-
-	if (set.find(sum - root->data) != set.end()) {
-		cout << "Pair is found (" << sum - root->data
-			<< ", " << root->data << ")" << endl;
-		return true;
+	if (root == NULL) {
+		return;
 	}
-	else
-		set.insert(root->data);
-
-	return findpairUtil(root->right, sum, set);
+	inorder(root->left, v);
+	v.push_back(root->val);
+	inorder(root->right, v);
 }
 
-void findPair(Node* root, int sum)
+pair<int, int> findTarget(TreeNode* root, int k)
 {
-	unordered_set<int> set;
-	if (!findpairUtil(root, sum, set))
-		cout << "Pairs do not exit" << endl;
-}
 
+	vector<int> v;
+	inorder(root, v);
+	int n = v.size();
+	int i = 0;
+	int j = n - 1;
+	while (j > i) {
+		if (v[i] + v[j] == k) {
+			return { v[i], v[j] };
+		}
+		else if (v[i] + v[j] > k) {
+			j--;
+		}
+		else {
+			i++;
+		}
+	}
+	return { -1, -1 };
+}
 
 int main()
 {
-	Node* root = NULL;
+
+	TreeNode* root = NULL;
 	root = insert(root, 15);
 	root = insert(root, 10);
 	root = insert(root, 20);
@@ -76,8 +160,8 @@ int main()
 	root = insert(root, 25);
 	root = insert(root, 10);
 
-	int sum = 28;
-	findPair(root, sum);
+	int k = 28;
 
-	return 0;
+	auto a = findTarget(root, k);
+	cout << a.first << " " << a.second << endl;
 }
