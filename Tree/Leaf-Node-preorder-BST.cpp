585392bad58d02b4ca882e2034511d73b9775924
AbirChoudhury-26 <abirudaipur@gmail.com>
2024-05-18 18:23:47 +0530

    New

--- Tree/Leaf-Node-preorder-BST.cpp
+++ Tree/Leaf-Node-preorder-BST.cpp
@@ -1,3 +1,9 @@
+/* Simple Approach(Recursion based):
+
+The idea is to use two min and max variables and taking i (index in input array), the index for given preorder array, and recursively creating root node and correspondingly checking if left and right are existing or not. 
+This method returns boolean variable, and if both left and right are false it simply means that left and right are null hence it must be a leaf node so print it right there and return back true as root at that index existed.
+*/
+
 #include<bits/stdc++.h>
 using namespace std;
 
