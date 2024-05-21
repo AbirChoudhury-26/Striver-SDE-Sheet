623dbe9f2b8805ef8b4228394107128e4d70cf4c
AbirChoudhury-26 <abirudaipur@gmail.com>
2024-05-21 21:43:47 +0530

    New

--- Tree/Inorder-Traversal-Optimise.cpp
+++ Tree/Inorder-Traversal-Optimise.cpp
@@ -1,3 +1,17 @@
+// Approach 1: Using Stack
+
+/*
+
+Create an empty stack (say S).
+Initialize the current node as root.
+Push the current node to S and set current = current->left until current is NULL
+If current is NULL and the stack is not empty then:
+Pop the top item from the stack.
+Print the popped item and set current = popped_item->right 
+Go to step 3.
+If current is NULL and the stack is empty then we are done.
+*/
+
 #include <bits/stdc++.h>
 using namespace std;
 
